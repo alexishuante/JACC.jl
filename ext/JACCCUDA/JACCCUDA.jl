165b1c8d0881dc5efb5f80e47f2e33b4ee5dc937
alexishuante <140995548+alexishuante@users.noreply.github.com>
2024-09-26 20:50:43 -0500

    Update JACCCUDA.jl

--- ext/JACCCUDA/JACCCUDA.jl
+++ ext/JACCCUDA/JACCCUDA.jl
@@ -22,6 +22,7 @@ function JACC.parallel_for(N::I, f::F, x...) where {I <: Integer, F <: Function}
     maxPossibleThreads = 512
     threads = min(N, maxPossibleThreads)
     blocks = ceil(Int, N / threads)
+    println("Using parameters: <<<$(blocks),$(threads)>>>")
     shmem_size = attribute(device(),CUDA.DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK)
     #parallel_kernel(parallel_kargs...; threads = threads, blocks = blocks)
     CUDA.@sync @cuda threads = threads blocks = blocks shmem = shmem_size _parallel_for_cuda(N, f, x...)
