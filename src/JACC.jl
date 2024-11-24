1d7a9111b2c07acf4c6f700e59a93113efa8cdfb
alexishuante <140995548+alexishuante@users.noreply.github.com>
2024-11-24 00:06:06 -0600

    Update JACC.jl

--- src/JACC.jl
+++ src/JACC.jl
@@ -1,46 +1,181 @@
 
-module JACC
+# module JACC
 
+# import Atomix: @atomic
+
+# # module to set backend preferences 
+# include("JACCPreferences.jl")
+
+# default_backend() = get_backend(JACCPreferences._backend_dispatchable)
+
+# struct ThreadsBackend end
+
+# include("helper.jl")
+# # overloaded array functions
+# include("array.jl")
+
+# include("JACCBLAS.jl")
+# using .BLAS
+
+# include("JACCMULTI.jl")
+# using .Multi
+
+# include("JACCEXPERIMENTAL.jl")
+# using .Experimental
+
+# get_backend(::Val{:threads}) = ThreadsBackend()
+
+# export Array, @atomic
+# export parallel_for
+# export parallel_reduce
+
+# global Array
+
+# function parallel_for(
+#         ::ThreadsBackend, N::I, f::F, x...) where {I <: Integer, F <: Function}
+#     @maybe_threaded for i in 1:N
+#         f(i, x...)
+#     end
+# end
+
+# function parallel_for(
+#         ::ThreadsBackend, (M, N)::Tuple{I, I}, f::F, x...) where {
+#         I <: Integer, F <: Function}
+#     @maybe_threaded for j in 1:N
+#         for i in 1:M
+#             f(i, j, x...)
+#         end
+#     end
+# end
+
+# function parallel_for(
+#         ::ThreadsBackend, (L, M, N)::Tuple{I, I, I}, f::F,
+#         x...) where {
+#         I <: Integer, F <: Function}
+#     # only threaded at the first level (no collapse equivalent)
+#     @maybe_threaded for k in 1:N
+#         for j in 1:M
+#             for i in 1:L
+#                 f(i, j, k, x...)
+#             end
+#         end
+#     end
+# end
+
+# function parallel_reduce(
+#         ::ThreadsBackend, N::Integer, op, f::Function, x...; init)
+#     ret = init
+#     tmp = fill(init, Threads.nthreads())
+#     @maybe_threaded for i in 1:N
+#         tmp[Threads.threadid()] = op.(tmp[Threads.threadid()], f(i, x...))
+#     end
+#     for i in 1:Threads.nthreads()
+#         ret = op.(ret, tmp[i])
+#     end
+#     return ret
+# end
+
+# function parallel_reduce(
+#         ::ThreadsBackend, (M, N)::Tuple{Integer, Integer}, op, f::Function, x...; init)
+#     ret = init
+#     tmp = fill(init, Threads.nthreads())
+#     @maybe_threaded for j in 1:N
+#         for i in 1:M
+#             tmp[Threads.threadid()] = op.(
+#                 tmp[Threads.threadid()], f(i, j, x...))
+#         end
+#     end
+#     for i in 1:Threads.nthreads()
+#         ret = op.(ret, tmp[i])
+#     end
+#     return ret
+# end
+
+# array_type(::ThreadsBackend) = Base.Array{T, N} where {T, N}
+
+# function shared(x::Base.Array{T, N}) where {T, N}
+#     return x
+# end
+
+# struct Array{T, N} end
+# function (::Type{Array{T, N}})(args...; kwargs...) where {T, N}
+#     array_type(){T, N}(args...; kwargs...)
+# end
+# function (::Type{Array{T}})(args...; kwargs...) where {T}
+#     array_type(){T}(args...; kwargs...)
+# end
+# (::Type{Array})(args...; kwargs...) = array_type()(args...; kwargs...)
+
+# array_type() = array_type(default_backend())
+
+# function parallel_for(N::I, f::F, x...) where {I <: Integer, F <: Function}
+#     return parallel_for(default_backend(), N, f, x...)
+# end
+
+# function parallel_for(
+#         (M, N)::Tuple{I, I}, f::F, x...) where {I <: Integer, F <: Function}
+#     return parallel_for(default_backend(), (M, N), f, x...)
+# end
+
+# function parallel_for((L, M, N)::Tuple{I, I, I}, f::F,
+#         x...) where {I <: Integer, F <: Function}
+#     return parallel_for(default_backend(), (L, M, N), f, x...)
+# end
+
+# function parallel_reduce(
+#         N::I, op, f::F, x...; init) where {I <: Integer, F <: Function}
+#     return parallel_reduce(default_backend(), N, op, f, x...; init = init)
+# end
+
+# function parallel_reduce(N::Integer, f::Function, x...)
+#     return parallel_reduce(N, +, f, x...; init = zero(Float64))
+# end
+
+# function parallel_reduce((M, N)::Tuple{I, I}, op, f::F, x...;
+#         init) where {I <: Integer, F <: Function}
+#     return parallel_reduce(default_backend(), (M, N), op, f, x...; init = init)
+# end
+
+# function parallel_reduce((M, N)::Tuple{Integer, Integer}, f::Function, x...)
+#     return parallel_reduce((M, N), +, f, x...; init = zero(Float64))
+# end
+
+# end # module JACC
+
+
+module JACC
 import Atomix: @atomic
 
 # module to set backend preferences 
 include("JACCPreferences.jl")
 
-default_backend() = get_backend(JACCPreferences._backend_dispatchable)
-
-struct ThreadsBackend end
+# Define and use a single global backend
+const BACKEND = ThreadsBackend()
 
 include("helper.jl")
-# overloaded array functions
 include("array.jl")
-
 include("JACCBLAS.jl")
 using .BLAS
-
 include("JACCMULTI.jl")
 using .Multi
-
 include("JACCEXPERIMENTAL.jl")
 using .Experimental
-
-get_backend(::Val{:threads}) = ThreadsBackend()
 
 export Array, @atomic
 export parallel_for
 export parallel_reduce
 
-global Array
+# Direct array type definition
+const Array = Base.Array
 
-function parallel_for(
-        ::ThreadsBackend, N::I, f::F, x...) where {I <: Integer, F <: Function}
+# Direct parallel operations without backend dispatch
+function parallel_for(N::I, f::F, x...) where {I <: Integer, F <: Function}
     @maybe_threaded for i in 1:N
         f(i, x...)
     end
 end
 
-function parallel_for(
-        ::ThreadsBackend, (M, N)::Tuple{I, I}, f::F, x...) where {
-        I <: Integer, F <: Function}
+function parallel_for((M, N)::Tuple{I, I}, f::F, x...) where {I <: Integer, F <: Function}
     @maybe_threaded for j in 1:N
         for i in 1:M
             f(i, j, x...)
@@ -48,11 +183,7 @@ function parallel_for(
     end
 end
 
-function parallel_for(
-        ::ThreadsBackend, (L, M, N)::Tuple{I, I, I}, f::F,
-        x...) where {
-        I <: Integer, F <: Function}
-    # only threaded at the first level (no collapse equivalent)
+function parallel_for((L, M, N)::Tuple{I, I, I}, f::F, x...) where {I <: Integer, F <: Function}
     @maybe_threaded for k in 1:N
         for j in 1:M
             for i in 1:L
@@ -62,8 +193,8 @@ function parallel_for(
     end
 end
 
-function parallel_reduce(
-        ::ThreadsBackend, N::Integer, op, f::Function, x...; init)
+# Direct reduction operations
+function parallel_reduce(N::Integer, op, f::Function, x...; init)
     ret = init
     tmp = fill(init, Threads.nthreads())
     @maybe_threaded for i in 1:N
@@ -75,14 +206,12 @@ function parallel_reduce(
     return ret
 end
 
-function parallel_reduce(
-        ::ThreadsBackend, (M, N)::Tuple{Integer, Integer}, op, f::Function, x...; init)
+function parallel_reduce((M, N)::Tuple{Integer, Integer}, op, f::Function, x...; init)
     ret = init
     tmp = fill(init, Threads.nthreads())
     @maybe_threaded for j in 1:N
         for i in 1:M
-            tmp[Threads.threadid()] = op.(
-                tmp[Threads.threadid()], f(i, j, x...))
+            tmp[Threads.threadid()] = op.(tmp[Threads.threadid()], f(i, j, x...))
         end
     end
     for i in 1:Threads.nthreads()
@@ -91,53 +220,17 @@ function parallel_reduce(
     return ret
 end
 
-array_type(::ThreadsBackend) = Base.Array{T, N} where {T, N}
-
-function shared(x::Base.Array{T, N}) where {T, N}
-    return x
-end
-
-struct Array{T, N} end
-function (::Type{Array{T, N}})(args...; kwargs...) where {T, N}
-    array_type(){T, N}(args...; kwargs...)
-end
-function (::Type{Array{T}})(args...; kwargs...) where {T}
-    array_type(){T}(args...; kwargs...)
-end
-(::Type{Array})(args...; kwargs...) = array_type()(args...; kwargs...)
-
-array_type() = array_type(default_backend())
-
-function parallel_for(N::I, f::F, x...) where {I <: Integer, F <: Function}
-    return parallel_for(default_backend(), N, f, x...)
-end
-
-function parallel_for(
-        (M, N)::Tuple{I, I}, f::F, x...) where {I <: Integer, F <: Function}
-    return parallel_for(default_backend(), (M, N), f, x...)
-end
-
-function parallel_for((L, M, N)::Tuple{I, I, I}, f::F,
-        x...) where {I <: Integer, F <: Function}
-    return parallel_for(default_backend(), (L, M, N), f, x...)
-end
-
-function parallel_reduce(
-        N::I, op, f::F, x...; init) where {I <: Integer, F <: Function}
-    return parallel_reduce(default_backend(), N, op, f, x...; init = init)
-end
-
 function parallel_reduce(N::Integer, f::Function, x...)
-    return parallel_reduce(N, +, f, x...; init = zero(Float64))
-end
-
-function parallel_reduce((M, N)::Tuple{I, I}, op, f::F, x...;
-        init) where {I <: Integer, F <: Function}
-    return parallel_reduce(default_backend(), (M, N), op, f, x...; init = init)
+    parallel_reduce(N, +, f, x...; init = zero(Float64))
 end
 
 function parallel_reduce((M, N)::Tuple{Integer, Integer}, f::Function, x...)
-    return parallel_reduce((M, N), +, f, x...; init = zero(Float64))
+    parallel_reduce((M, N), +, f, x...; init = zero(Float64))
+end
+
+# Direct array sharing
+function shared(x::Base.Array{T, N}) where {T, N}
+    return x
 end
 
 end # module JACC
